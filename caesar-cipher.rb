84538f3de5445bc15820bbf818e75c4e56735cbe
Vladimir <duenasvladimir@gmail.com>
2022-08-17 12:48:07 -0600

    code refactor

--- caesar-cipher.rb
+++ caesar-cipher.rb
@@ -1,22 +1,22 @@
-def get_letter(position, is_upcase = false)
-  if is_upcase then
-    (position + "A".ord).chr
+def get_letter(position, is_upcase: false)
+  if is_upcase
+    (position + 'A'.ord).chr
   else
-    (position + "a".ord).chr
+    (position + 'a'.ord).chr
   end
 end
 
-def get_alphabet_downcase() 
-  Array.new(26).map.with_index {|letter, index| get_letter(index)}
+def alphabet_downcase
+  Array.new(26).map.with_index { |_, index| get_letter(index) }
 end
 
-def get_alphabet_upcase()
-  Array.new(26).map.with_index {|letter, index| get_letter(index, true)}
+def alphabet_upcase
+  Array.new(26).map.with_index { |_, index| get_letter(index, true) }
 end
 
-def is_punctuation?(letter) 
+def punctuation?(letter)
   result = false
-  punctuation = ["\s", ".", "!", "?", "\t"]
+  punctuation = ["\s", ',', '.', '!', '?', "\t"]
   punctuation.each { |mark| result = true if mark == letter }
   result
 end
@@ -24,26 +24,24 @@ end
 def caesar_cipher(sentence, shift)
   encrypted = []
   fixed_index = 0
-  alphabet_downcase = get_alphabet_downcase
-  alphabet_upcase = get_alphabet_upcase
+  alphabet_down = alphabet_downcase
+  alphabet_up = alphabet_upcase
 
-  sentence.split("").each do |sentence_letter|
-    encrypted.push(sentence_letter) if is_punctuation?(sentence_letter)
-  
-    alphabet_downcase.each.with_index do |letter, index|
+  sentence.split('').each do |sentence_letter|
+    encrypted.push(sentence_letter) if punctuation?(sentence_letter)
+    alphabet_down.each.with_index do |letter, index|
       fixed_index = index + shift
       fixed_index -= 26 unless fixed_index.between?(0, 26)
 
-      encrypted.push(alphabet_downcase[fixed_index]) if sentence_letter == letter
+      encrypted.push(alphabet_down[fixed_index]) if sentence_letter == letter
     end
 
-    alphabet_upcase.each.with_index do |letter, index|
+    alphabet_up.each.with_index do |letter, index|
       fixed_index = index + shift
       fixed_index -= 26 unless fixed_index.between?(0, 26)
 
-      encrypted.push(alphabet_upcase[fixed_index]) if sentence_letter == letter
+      encrypted.push(alphabet_up[fixed_index]) if sentence_letter == letter
     end
   end
   encrypted.join
 end
-
